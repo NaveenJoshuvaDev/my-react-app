a9b6cf949da7cbe752384ef0ceeead4751e99234
NaveenJoshuvaDev <njworkholic@gmail.com>
2023-05-09 17:59:03 +0530

    rendering list

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,45 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-let globalID=0;
-function App() {
-  const [task, setTask] = useState("")
-  const [todos, setTodos] = useState([])
-  function createTodo(event) {
-   //console.log(`Task Value: ${task}`);
-   event.preventDefault()
-   setTodos(oldTodos => {
-    setTask("")
-    return [...oldTodos, {todo: task, id: globalID++}]
-   })
-  }
-  // function tryToCheck(e) {
-  //   if(e.keyCode=== 13) {
-  //     createTodo()
-  //   }
-  // }
-  function deleteitem(itemID) {
-   setTodos(oldTodos => oldTodos.filter(item=>item.id !== itemID))
-  }
-  return (
-    <div className="App">
-     <h1>Best todo App Ever</h1>
-     <form onSubmit={createTodo}>
-         <input  type="text" value={task} onChange={event => {
-            setTask(event.target.value)
-          }}/>
-          <button type="submit" onClick={createTodo}>Create Todo</button>
-    </form>
-     <ul>
-     {todos.map((item, index) =>{
-      return <div  key={item.id}><li>{item.todo}({item.id})</li>
-      <button onClick={() => deleteitem(item.id)}>Delete</button>
-      </div>
-     })
+import React, { useState } from 'react';
 
-     }
-     </ul>
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
